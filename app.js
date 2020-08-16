b0eb8a508d43683aac8d5850c6097e3ef40245eb
ThePsyk3y <50011738+ThePsyk3y@users.noreply.github.com>
2020-08-17 03:15:50 +0530

    Moved button class id from controller function to UIctrl function.

--- app.js
+++ app.js
@@ -10,25 +10,26 @@ const UIController = (function UI() {
     typeID: '.add__type',
     descID: '.add__description',
     valID: '.add__value',
+    btnID: '.add__btn',
   };
 
   return {
     getInput() {
       return {
-        type: document.querySelector(UIclassID.type).value, // ?Retruns Income or Expense
+        type: document.querySelector(UIclassID.typeID).value, // ?Retruns Income or Expense
         desc: document.querySelector(UIclassID.descID).value, // ?Returns description.
         val: document.querySelector(UIclassID.valID).value, // ?Returns entered value
       };
+    },
+    getClassID() {
+      return UIclassID;
     },
   };
 }());
 
 // *App Controller
 const controller = (function ctrl(budgetCtrl, UICtrl) {
-  const ctrlClassID = {
-    btnID: '.add__btn',
-  };
-
+  const ctrlClassID = UICtrl.getClassID();
   const ctrlAddItem = function add() {
     const input = UICtrl.getInput();
     console.log(input);
